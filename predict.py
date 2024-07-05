43cf0c8395faa9da1e21eb4ea7c61312f812087e
Aditya <99386923+Aditya-0114@users.noreply.github.com>
2024-07-05 15:46:33 +0530

    Prediction Script

---
+++ predict.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+model=YOLO("yolov8m-seg-custom.pt")
+
+# model.predict(source="test.mp4", show=True, save=True, show_labels=True, show_conf=True, conf=0.5, save_txt=False, save_crop=False,  line_width=1)
+
+model.export(format="tflite")
